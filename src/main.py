ab8e13a7898122edf5d8da6c6b92c5a375aebcb5
JuanAntonaccio <jmantonaccio@gmail.com>
2021-10-26 01:04:06 +0000

    con problemas en post

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import json
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
@@ -39,6 +40,74 @@ def get_all_users():
         return jsonify("no se encontraron usuarios"),404
         
     return jsonify(usuarios), 200
+
+@app.route('/personajes', methods=['GET'])
+def get_all_personajes():
+
+    personajes=Personaje.query.all()
+    personajes = list(map(lambda per: per.serialize(), personajes ))
+    if not personajes:
+        return jsonify("no se encontraron personajes"),404
+        
+    return jsonify(personajes), 200    
+
+@app.route('/personajes', methods=['POST'])
+def add_personaje():
+    # primero leo lo que viene en el body
+    body_personaje=json.loads(request.data)
+    # print (body_personaje)
+    return jsonify()
+    
+    db.session.add_personaje()                
+    db.session.commit()
+    
+
+
+
+@app.route('/personajes/<int:id>', methods=['PUT'])
+def update_personaje():
+    body=json.loads(request.data)
+    personaje=Personaje.query.get(id)
+    if personaje is None:
+        raise APIException('Personaje no encontrado',status_code=404)
+    if "name" in body:
+        personaje.name=body['name']
+    if "height" in body:
+        personaje.height=body['height']  
+    if "mass" in body:
+        personaje.mass=body['mass']
+    if "hair_color" in body:
+        personaje.hair_color=body['hair_color'] 
+    if "skin_color" in body:
+        personaje.skin_color=body['skin_color']    
+    if "eye_color" in body:
+        personaje.eye_color=body['eye_color']    
+    if "birth_year" in body:
+        personaje.birth_year=body['birth_year']                 
+    db.session.commit()
+    personajes=Personaje.query.all()
+    personajes = list(map(lambda per: per.serialize(), personajes ))
+    if not personajes:
+        return jsonify("no se encontraron personajes"),404
+        
+    return jsonify(personajes), 200  
+
+
+@app.route('/personajes/<int:id>', methods=['DELETE'])
+def delete_personaje():
+    
+    personaje=Personaje.query.get(id)
+    if personaje is None:
+        raise APIException('Personaje no encontrado',status_code=404)
+    db.session.delete(personaje)                
+    db.session.commit()
+    personajes=Personaje.query.all()
+    personajes = list(map(lambda per: per.serialize(), personajes ))
+    if not personajes:
+        return jsonify("no se encontraron personajes"),404
+        
+    return jsonify(personajes), 200  
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
